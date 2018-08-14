04147c93c544df9658f765d728740c3b2469504b
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2018-08-14 10:29:22 +0200

    derive(PartialEq) for CopyState

--- src/shared.rs
+++ src/shared.rs
@@ -6,7 +6,7 @@ use statrs::distribution::{StudentsT, Univariate};
 
 use super::stats::*;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum CopyState {
     Deletion,
     Neutral,
